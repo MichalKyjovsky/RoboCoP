7c54b2795731ae860eb777c524d776d816b80b3f
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-08-10 20:12:58 +0200

    Documentation update.

--- src/main/java/cz/cuni/mff/kyjovsm/robocop/RobotFrameworkLanguage.java
+++ src/main/java/cz/cuni/mff/kyjovsm/robocop/RobotFrameworkLanguage.java
@@ -2,9 +2,18 @@ package cz.cuni.mff.kyjovsm.robocop;
 
 import com.intellij.lang.Language;
 
+/**
+ *
+ */
 public class RobotFrameworkLanguage extends Language {
+    /**
+     *
+     */
     public static final RobotFrameworkLanguage INSTANCE = new RobotFrameworkLanguage();
 
+    /**
+     *
+     */
     private RobotFrameworkLanguage() {
         super("RobotFramework");
     }
