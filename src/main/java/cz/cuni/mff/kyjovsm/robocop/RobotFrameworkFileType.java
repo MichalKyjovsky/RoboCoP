35d094419dc9689cc0696e355ee5ffb6542eb3a9
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-08-10 20:12:58 +0200

    Documentation update.

--- src/main/java/cz/cuni/mff/kyjovsm/robocop/RobotFrameworkFileType.java
+++ src/main/java/cz/cuni/mff/kyjovsm/robocop/RobotFrameworkFileType.java
@@ -5,33 +5,54 @@ import cz.cuni.mff.kyjovsm.robocop.icons.RobotFrameworkIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
+import javax.swing.Icon;
 
+/**
+ *
+ */
 public class RobotFrameworkFileType extends LanguageFileType {
+    /**
+     *
+     */
     public static final RobotFrameworkFileType INSTANCE = new RobotFrameworkFileType();
 
+    /**
+     *
+     */
     private RobotFrameworkFileType() {
         super(RobotFrameworkLanguage.INSTANCE);
     }
 
+    /**
+     * @return
+     */
     @NotNull
     @Override
     public String getName() {
         return "Robot Framework File";
     }
 
+    /**
+     * @return
+     */
     @NotNull
     @Override
     public String getDescription() {
         return "Robot Framework file.";
     }
 
+    /**
+     * @return
+     */
     @NotNull
     @Override
     public String getDefaultExtension() {
         return "robot";
     }
 
+    /**
+     * @return
+     */
     @Nullable
     @Override
     public Icon getIcon() {
